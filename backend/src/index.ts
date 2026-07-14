9b9d3193992c7e45d10d6da48b9ee5acb9fdb1c9
bryan2251 <bryan.castr2251@gmail.com>
2026-07-13 21:21:24 -0400

    feat: deleting unused files and import function of those files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,6 @@ import express from "express";
 import cors from "cors";
 
 import taskRoutes from "./routes/task.routes";
-import authRoutes from "./routes/auth.routes";
 
 const app = express();
 const PORT = 3000;
@@ -13,7 +12,6 @@ app.use(express.json());
 
 // rutas
 app.use("/tasks", taskRoutes);
-app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`);
